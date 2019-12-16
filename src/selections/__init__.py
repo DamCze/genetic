3a758abda541b605142b9c94f128d3b93d1a5cd0
Damian Czech <damian5k6l@gmail.com>
2019-12-17 00:53:07 +0100

    Dodanie src/selections/__init__.py do importów

---
+++ src/selections/__init__.py
@@ -0,0 +1,5 @@
+from selections.tournament_selection import TournamentSelection
+from selections.best_selection import BestSelection
+from selections.roulette_wheel_selection import RouletteWheelSelection
+
+__all__ = ['TournamentSelection', 'BestSelection', 'RouletteWheelSelection']
